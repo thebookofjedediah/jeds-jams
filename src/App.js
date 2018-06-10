93282bedf699deffdb188e8474533ff13a94049a
Jedediah Arnold <jarnold910@gmail.com>
2018-06-10 16:23:14 -0400

    quick updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Bloc Jams</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/library'>Library</Link>
+          </nav>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album" component={ Album } />
+        </main>
       </div>
     );
   }
