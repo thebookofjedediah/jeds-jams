ac0bdba65a209b29af6c508265ec3a8b17f0d7e2
Jedediah Arnold <jarnold910@gmail.com>
2018-06-19 16:55:29 -0400

    checkpoint 6: actually completed this time

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/album" component={ Album } />
           <Route path="/album/:slug" component={Album} />
         </main>
       </div>
