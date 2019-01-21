179a0f1801accde539be19cf263824e8e77de1ba
Jedediah Arnold <jarnold910@gmail.com>
2019-01-21 12:21:36 -0500

    making it my own, final commit for now

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import './App.css';
-import Landing from './components/Landing';
-import Library from './components/Library';
-import Album from './components/Album';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/Landing";
+import Library from "./components/Library";
+import Album from "./components/Album";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <nav className="nav-bar">
           <header>
-            <Link to="/"><img src="/assets/images/bloc-jams-logo.png" alt="bloc jams logo" className="logo"></img></Link>
+            <Link to="/">
+              <img
+                src="/assets/images/jeds-jams.png"
+                alt="jed's jams logo"
+                className="logo"
+              />
+            </Link>
           </header>
-          <Link to='/' className="home-link" >Home</Link>
-          <Link to='/library' className="library-link" >Library</Link>
+          <Link to="/" className="home-link">
+            Home
+          </Link>
+          <Link to="/library" className="library-link">
+            Library
+          </Link>
         </nav>
         <main>
           <Route exact path="/" component={Landing} />
